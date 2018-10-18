bfcd938bd2216f7c22093603b4d2ca084f3bad3a
ptstory <perry.t.story@gmail.com>
2018-10-18 17:01:36 -0400

    refactor files into subdirectories

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
-        <script type="text/javascript" src="typeahead-helper.js"></script>
-        <script type="text/javascript" src="bootstrap3-typeahead.js"></script>
+        <script type="text/javascript" src="./resources/js/typeahead-helper.js"></script>
+        <script type="text/javascript" src="./resources/js/bootstrap3-typeahead.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="index_style.css" />
+        <link rel="stylesheet" href="./resources/css/index_style.css" />
     </head>
     
     <body background-color: #f5f6f7>
@@ -22,52 +22,52 @@
         </div>
 
         <div class="div-logo">
-            <img src='logo.png'>
+            <img src='./resources/images/logo.png'>
             <p class="h5 txtC">
             Crime Trends in 2017
           </p>
         </div>
 <div class="crime-box-full-width-fixed crime-box crime-box-full-width backgroundLowlight pvl clearfix">
                           <div class="crime-box-col floatLeft txtC brs">
-                             <p class="h1 mbn typeEmphasize crime-text-0"><a class="h1 mbn typeEmphasize crime-text-0" href="total-crimes-reported.html">26,759</a>
+                             <p class="h1 mbn typeEmphasize crime-text-0"><a class="h1 mbn typeEmphasize crime-text-0" href="./resources/total-crimes-reported.html">26,759</a>
                             <p class="h7 mtn typeCaps typeEmphasize">TOTAL CRIMES REPORTED</p>
               </div>
                            <div class="crime-box-col floatLeft txtC brs ">
-                            <p class="h1 mbn typeEmphasize crime-text-4"><a class="h1 mbn typeEmphasize crime-text-4" href="highest-crime-month.html">October</a>
+                            <p class="h1 mbn typeEmphasize crime-text-4"><a class="h1 mbn typeEmphasize crime-text-4" href="./resources/highest-crime-month.html">October</a>
 </p>
                             <p class="h7 mtn typeCaps typeEmphasize">Highest Crime Month</p>
               </div>
                                <div class="crime-box-col floatLeft txtC brs ">
-                            <p class="h1 mbn typeEmphasize crime-text-4"><a class="h1 mbn typeEmphasize crime-text-4" href="highest-crime-day.html">Monday</a>
+                            <p class="h1 mbn typeEmphasize crime-text-4"><a class="h1 mbn typeEmphasize crime-text-4" href="./resources/highest-crime-day.html">Monday</a>
                             <p class="h7 mtn typeCaps typeEmphasize">Highest Crime Day</p>
               </div>
                                <div class="crime-box-col floatLeft txtC brs ">
-                            <p class="h1 mbn typeEmphasize crime-text-4"><a class="h1 mbn typeEmphasize crime-text-4" href="highest-crime-beat.html">211</a>
+                            <p class="h1 mbn typeEmphasize crime-text-4"><a class="h1 mbn typeEmphasize crime-text-4" href="./resources/highest-crime-beat.html">211</a>
                             <p class="h7 mtn typeCaps typeEmphasize">Highest Crime Beat</p>
               </div>
                           <div class="crime-box-col floatLeft txtC ">
-                             <p class="h1 mbn typeEmphasize crime-text-0"><a class="h1 mbn typeEmphasize crime-text-0" href="highest-crime-neighborhood.html">Downtown</a>
+                             <p class="h1 mbn typeEmphasize crime-text-0"><a class="h1 mbn typeEmphasize crime-text-0" href="./resources/highest-crime-neighborhood.html">Downtown</a>
                             <p class="h7 mtn typeCaps typeEmphasize">Highest Crime Neighborhood</p>
               </div>
            <div class="crime-box-col floatLeft txtC brs">
-            <p class="h1 mbn typeEmphasize crime-text-1"><a class="h1 mbn typeEmphasize crime-text-1" href="nonviolent-violent-ratio.html">6 : 1</a>
+            <p class="h1 mbn typeEmphasize crime-text-1"><a class="h1 mbn typeEmphasize crime-text-1" href="./resources/nonviolent-violent-ratio.html">6 : 1</a>
              <p class="h7 mtn typeCaps typeEmphasize">Non-Violent : Violent</p>
               </div>
            <div class="crime-box-col floatLeft txtC brs">
-             <p class="h1 mbn typeEmphasize crime-text-5"><a class="h1 mbn typeEmphasize crime-text-5" href="lowest-crime-month.html">March</a>
+             <p class="h1 mbn typeEmphasize crime-text-5"><a class="h1 mbn typeEmphasize crime-text-5" href="./resources/lowest-crime-month.html">March</a>
 
                 <p class="h7 mtn typeCaps typeEmphasize">Lowest Crime Month</p>
               </div>
            <div class="crime-box-col floatLeft txtC brs">
-             <p class="h1 mbn typeEmphasize crime-text-5"><a class="h1 mbn typeEmphasize crime-text-5" href="lowest-crime-day.html">Friday</a>
+             <p class="h1 mbn typeEmphasize crime-text-5"><a class="h1 mbn typeEmphasize crime-text-5" href="./resources/lowest-crime-day.html">Friday</a>
                 <p class="h7 mtn typeCaps typeEmphasize">Lowest Crime Day</p>
               </div>
            <div class="crime-box-col floatLeft txtC brs">
-             <p class="h1 mbn typeEmphasize crime-text-5"><a class="h1 mbn typeEmphasize crime-text-5" href="lowest-crime-beat.html">707</a>
+             <p class="h1 mbn typeEmphasize crime-text-5"><a class="h1 mbn typeEmphasize crime-text-5" href="./resources/lowest-crime-beat.html">707</a>
                 <p class="h7 mtn typeCaps typeEmphasize">Lowest Crime Beat</p>
               </div>
            <div class="crime-box-col floatLeft txtC">
-            <p class="h1 mbn typeEmphasize crime-text-1"><a class="h1 mbn typeEmphasize crime-text-1" href="larceny-gsu.html">Sunday</a>
+            <p class="h1 mbn typeEmphasize crime-text-1"><a class="h1 mbn typeEmphasize crime-text-1" href="./resources/larceny-gsu.html">Sunday</a>
              <p class="h7 mtn typeCaps typeEmphasize">Day Most Likely To Be A Victim of<br /> Larceny At GSU</p>
               </div>
                       </div>
